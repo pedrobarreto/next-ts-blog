f7d3df8d9d95121c3a550db5cb8b761ae323f480
Pedro <pedro@Pedros-MacBook-Pro.local>
2022-04-12 09:20:42 -0300

    adjusting service to implement infinite scroll

--- services/DevApi.ts
+++ services/DevApi.ts
@@ -1,15 +1,12 @@
-
 import axios, { AxiosResponse, AxiosRequestHeaders} from 'axios'
 
-export const getDevApi = async (endpoint:string) => {
+export const getDevApi = async (endpoint:string, page: number) => {
   // const params = { username:process.env.USER, per_page: 10  }
-  const params = { }
+  const params = { per_page: 6, page: page}
   const headers:AxiosRequestHeaders = { api_key: process.env.APIKEY }
   const {data}: AxiosResponse = await axios.get(`https://dev.to/api/${endpoint}`, {
       params,
       headers,
   })
-  return data 
+  return data
 }
-
-getDevApi('articles')
