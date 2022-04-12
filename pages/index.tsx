a38681e6b067dfcd8ca75b005d28d526e408a1a8
Pedro <pedro@Pedros-MacBook-Pro.local>
2022-04-12 09:40:35 -0300

    react-infinite-scroll-component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,6 @@ import type { NextPage } from 'next'
 import { GetStaticProps } from 'next'
 import { getDevApi } from '../services/DevApi'
 import PostCard from '../components/PostCard'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = ({posts}) => {
   return (
