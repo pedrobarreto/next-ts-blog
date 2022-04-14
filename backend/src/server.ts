759578dc5bccd5b42520f14dc4c713e81a7344a6
Pedro <pedro@Pedros-MacBook-Pro.local>
2022-04-14 20:09:00 -0300

    fix type error getDevApi

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,10 @@ import getPosts from './controllers/blogPostsController';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
