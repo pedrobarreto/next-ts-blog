359a35d6a2cf1db7228ce6380ab82cc1556a000a
Pedro <pedro@Pedros-MacBook-Pro.local>
2022-04-14 20:02:07 -0300

    fix dev api service

--- backend/src/services/DevApi.ts
+++ backend/src/services/DevApi.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse, AxiosRequestHeaders} from 'axios'
 
 export const getDevApi = async ({ endpoint, pagination }:any) => {
   const params = { per_page: 6, page: pagination, }
-  const headers:AxiosRequestHeaders = { api_key: "REUscynvP8geeyzoLY4WN6qy" }
+  const headers:AxiosRequestHeaders = { api_key: process.env.APIKEY  }
   const {data}: AxiosResponse = await axios.get(`https://dev.to/api/${endpoint}`, {
       params,
       headers,
