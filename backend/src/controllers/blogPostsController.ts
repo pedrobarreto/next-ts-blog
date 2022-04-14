ffd0702a4500edf5a4e7a2807f69608db65004cb
Pedro <pedro@Pedros-MacBook-Pro.local>
2022-04-14 19:08:24 -0300

    blogs endpoint

--- backend/src/controllers/blogPostsController.ts
+++ backend/src/controllers/blogPostsController.ts
@@ -1,6 +1,8 @@
 import { getDevApi } from "../services/DevApi";
 
-const getPosts = async (req, res) => {
+const getPosts = async (req:any, res:any) => {
   const posts = await getDevApi('articles', 1 )
   res.status(200).json(posts);
 };
+
+export default getPosts;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import getPosts from './controllers/blogPostsController';
 
 const app = express();
 const port = 3001;
 
 
+app.get('/posts', getPosts);
 app.listen(port, () => console.log(`funcionando na posta: ${port}!`));
   
